c0e2d312720c50aa0a8deecd38f0590a53e6c98f
Ivan Art <etherial.man@gmail.com>
2024-09-13 11:52:35 +0500

    Опции подключения к RabbitMQ из конфига

---
+++ EnviBad.ReportMaker.Common/Models/Options/MassTransitOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace EnviBad.ReportMaker.Common.Models.Options
+{
+    /// <summary>
+    /// Настройки подключения к MQ и работы MassTransit
+    /// </summary>
+    public class MassTransitOptions
+    {
+        public string? RabbitUser { get; set; }
+        public string? RabbitPassword { get; set; }
+        public string? RabbitHost { get; set; }
+        public ushort? RabbitPort { get; set; }
+    }
+}
--- EnviBad.ReportMaker.Worker/Program.cs
+++ EnviBad.ReportMaker.Worker/Program.cs
@@ -1,5 +1,7 @@
-﻿using EnviBad.ReportMaker.Core.MqConsumers;
+﻿using EnviBad.ReportMaker.Common.Models.Options;
+using EnviBad.ReportMaker.Core.MqConsumers;
 using MassTransit;
+using Microsoft.Extensions.Configuration;
 
 namespace EnviBad.ReportMaker.Worker
 {
@@ -8,14 +10,20 @@ namespace EnviBad.ReportMaker.Worker
         static async Task Main(string[] args)
         {
             Console.WriteLine("Report Consumer started.");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
 
+            IConfiguration config = builder.Build();
+
+            var rabbitSettings = config.GetSection("MassTransitOptions").Get<MassTransitOptions>();
             IBusControl busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
             {
                 // TODO: Get from options
-                cfg.Host("127.0.0.1", 5672, "/envibad", h =>
+                cfg.Host(rabbitSettings?.RabbitHost, rabbitSettings?.RabbitPort ?? 5672, "/envibad", h =>
                 {
-                    h.Username("mq");
-                    h.Password("123");
+                    h.Username(rabbitSettings?.RabbitUser ?? "guest");
+                    h.Password(rabbitSettings?.RabbitPassword ?? "guest");
                 });
 
                 cfg.ReceiveEndpoint("report-request-created-event", e =>
